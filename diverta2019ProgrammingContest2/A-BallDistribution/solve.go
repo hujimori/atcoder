b6d671f4d7e3a7e79e9f941e47a51490d5644c51
hujimori <hujimori16@gmail.com>
2019-06-15 23:01:53 +0900

    A-BallDistribution解答

---
+++ diverta2019ProgrammingContest2/A-BallDistribution/solve.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+
+	n := nextInt()
+	k := nextInt()
+
+	if k == 1 {
+		fmt.Println(0)
+	} else {
+		fmt.Println(n - k)
+	}
+
+}
