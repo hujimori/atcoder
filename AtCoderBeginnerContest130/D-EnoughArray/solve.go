d17794dfde3ef041c09556a4011405efa101caf2
hujimori <hujimori16@gmail.com>
2019-06-17 11:56:15 +0900

    D-EnoughArray時直し △

--- AtCoderBeginnerContest130/D-EnoughArray/solve.go
+++ AtCoderBeginnerContest130/D-EnoughArray/solve.go
@@ -32,27 +32,87 @@ func main() {
 	for i := 0; i < n; i++ {
 		slice[i] = nextInt()
 	}
-	count := 0
 
-	for i := 0; i < n-1; i++ {
-		sum := slice[i]
-		for j := i + 1; j < n; j++ {
-			fmt.Printf("%d + %d = %d\n", sum, slice[j], sum+slice[i])
-			sum += slice[j]
-			fmt.Printf("sum = %d\n", sum)
-			if sum >= k {
-				count += n - j
-				fmt.Printf("count %d = %d - %d\n", count, n, j)
+	answer := 0
+	sum := 0
 
+	r := 0
+
+	for l := 0; l < n; l++ {
+		for sum < k {
+			if r == n {
 				break
-
+			} else {
+				sum += slice[r]
+				r++
 			}
 		}
+
+		if sum < k {
+			break
+		}
+
+		answer += n - r + 1
+		sum -= slice[l]
+
 	}
+
+	fmt.Println(answer)
+	// var sum int
+	// for i := 0; i < n; i++ {
+	// 	slice[i] = nextInt()
+	// 	sum += slice[i]
+	// }
+	// count := 0
+
+	// sumA := 0
+
+	// for i := 0; i < n; i++ {
+
+	// 	sumA += slice[i]
+
+	// 	if sum-sumA < k {
+	// 		count += i
+
+	// 		fmt.Printf("count %d\n", count)
+	// 		break
+
+	// 	}
+
+	// }
+
+	// sumB := 0
+	// for i := 0; i < n; i++ {
+	// fmt.Printf("%d + %d = %d\n", sumB, slice[i], sumB+slice[i])
+
+	// 	sumB += slice[i]
+
+	// 	if sumB >= k {
+	// 		fmt.Printf("count %d = %d - %d\n", n-i, n, i)
+
+	// 		count += n - i
+	// 		break
+	// 	}
+
+	// }
+
+	// for i := 0; i < n-1; i++ {
+	// 	sum := 0
+	// 	for j := i; j < n; j++ {
+	// 		// fmt.Printf("%d + %d = %d\n", sum, slice[j], sum+slice[j])
+	// 		sum += slice[j]
+	// 		// fmt.Printf("sum = %d\n", sum)
+	// 		if sum >= k {
+	// 			count += n - j
+	// 			// fmt.Printf("count %d = %d - %d\n", count, n, j)
+	// 			break
+	// 		}
+	// 	}
+	// }
 
 	// if slice[len(slice)-1] >= k {
 	// 	count++
 	// }
 
-	fmt.Println(count)
+	// fmt.Println(count)
 }
