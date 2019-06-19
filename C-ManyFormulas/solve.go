36a6c745c90035482ac15937ecdb3b346340b81c
hujimori <hujimori16@gmail.com>
2019-06-19 11:55:33 +0900

    C-ManyFormulas解答 △

--- C-ManyFormulas/solve.go
+++ C-ManyFormulas/solve.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 )
 
 var sc = bufio.NewScanner(os.Stdin)
@@ -24,32 +23,75 @@ func nextLine() string {
 	return sc.Text()
 }
 
-var end int
-var slice []string
+// var end int
+// var slice []string
 
 func main() {
 	sc.Split(bufio.ScanWords)
 
-	slice = strings.Split(nextLine(), "")
-	end = len(slice)
+	// slice = strings.Split(nextLine(), "")
+	// end = len(slice)
 
-	ans := dfs(0, "")
+	s := nextLine()
+
+	// sum := 0
+	// for i := 1; i < len(s); i++ {
+	// 	s1 := s[0:i]
+	// 	fmt.Printf("s1=%s ", s1)
+	// 	a, _ := strconv.Atoi(s1)
+	// 	sum += a
+	// 	for j := i + 1; j < len(s); j++ {
+	// 		s2 := s[i:j]
+	// 		fmt.Printf("s2=%s ", s2)
+	// 		b, _ := strconv.Atoi(s2)
+	// 		sum += b
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	// fmt.Println(sum)
+
+	// fmt.Println(s)
+	// fmt.Println(s[0])
+	// fmt.Println(s[0:1])
+	// fmt.Println(s[0:2])
+	// fmt.Println(s[1:len(s)])
+	ans := dfs(0, s)
 	fmt.Println(ans)
 }
 
-func dfs(i int, ex string) string {
-	if i == end {
-		return ex
+func dfs(sum int, s string) int {
+	result := sum
+	for i := 1; i < len(s); i++ {
+		a, _ := strconv.Atoi(s[0:i])
+		result += dfs(sum+a, s[i:len(s)])
 	}
-
-	// i番目の後ろに+を挿入する場合
-	ex += dfs(i+1, slice[i]+"+")
-	fmt.Println(ex)
-
-	// i番目の後ろに+を挿入しない場合
-	ex += dfs(i+1, slice[i])
-
-	fmt.Println(ex)
-
-	return ex
+	a, _ := strconv.Atoi(s)
+	return result + a
 }
+
+// func dfs(sum int, s string) int {
+
+// 	if len(s) == 0 {
+// 		return sum
+// 	}
+
+// if i == len(s)+1 {
+// 	return sum
+// }
+
+// i番目の後ろに+を挿入する場合
+// var sum1 int
+// s1, _ := strconv.Atoi(s[0:1])
+
+// fmt.Printf("sum+s1=%d sum=%d s1=%d, s[i:len(s)]=%s\n", sum+s1, sum, s1, s[1:len(s)])
+// sum1 += dfs(sum+s1, s[1:len(s)])
+
+// i番目の後ろに+を挿入しない場合
+// var sum2 int
+// // s2, _ := strconv.Atoi(s[0:len(s)])
+// fmt.Printf("sum+s2=%d  sum=%d s2=%d, s[0:len(s)]=%s\n", sum+0, sum, 0, s[0:len(s)])
+// sum2 += dfs(sum, s[0:len(s)])
+
+// return sum2
+// }
