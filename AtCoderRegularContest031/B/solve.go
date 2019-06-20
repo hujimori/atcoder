c2390f94b4f44b53b52eff1bf68071b9d407384a
hujimori <hujimori16@gmail.com>
2019-06-20 14:10:31 +0900

    AtCoderRegularContest031-B問題解答

--- AtCoderRegularContest031/B/solve.go
+++ AtCoderRegularContest031/B/solve.go
@@ -3,73 +3,77 @@ package main
 import "fmt"
 
 var field []string
-var reached [][]bool
 
 var landCount int
-var h int
-var w int
 
 func main() {
 
-	h := 10
-	w := 10
+	field = make([]string, 10)
 
-	field = make([]string, h)
-	for i := 0; i < h; i++ {
+	for i := 0; i < 10; i++ {
 		fmt.Scan(&field[i])
 	}
 
-	for i := 0; i < h; i++ {
-		for j := 0; j < w; j++ {
-			if field[j][j] == 'o' {
-				landCount++
-			}
-		}
-	}
+	// for i := 0; i < 10; i++ {
+	// 	for j := 0; j < 10; j++ {
 
+	// 		fmt.Println(string(field[i][j]))
+	// 	}
+	// }
 	// L:
 
-	for i := 0; i < h; i++ {
-		for j := 0; j < w; j++ {
-			if field[i][j] == 'x' {
-				fmt.Printf("dfs=%d\n", dfs(i, j, 0))
-				// {
-				// fmt.Printf("landCount=%d\n", landCount)
-				// fmt.Println("YES")
-
-				// break L
-				// }
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			// fmt.Printf("i = %d, j = %d:", i, j)
+			if isOk(i, j) {
+				fmt.Println("YES")
+				return
 			}
 		}
 	}
 
-	fmt.Println("No")
+	fmt.Println("NO")
 
 }
 
-func dfs(x, y, count int) int {
-
-	if count != 0 && field[x][y] == 'x' {
-		fmt.Println(count)
-
-		return count
+func isOk(x, y int) bool {
+	reached := make([][]bool, 10)
+	for i := 0; i < 10; i++ {
+		reached[i] = make([]bool, 10)
 	}
 
-	if field[x][y] == 'o' {
-		count++
-	}
+	// if string(field[x][y]) == "x" {
+	dfs(x, y, reached)
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			if string(field[i][j]) == "o" && !reached[i][j] {
+				return false
+			}
+		}
 
-	if 0 <= x && x < w && 0 <= y && y < h {
-		return max(max(dfs(x+1, y, count), dfs(x-1, y, count)), max(dfs(x, y+1, count), dfs(x, y-1, count)))
 	}
+	// }
 
-	return count
+	return true
 }
 
-func max(a, b int) int {
-	if a < b {
-		return b
+var dx = []int{1, 0, 0, -1}
+var dy = []int{0, 1, -1, 0}
+
+func dfs(x, y int, reached [][]bool) {
+
+	reached[x][y] = true
+
+	for i := 0; i < len(dx); i++ {
+
+		nx := x + dx[i]
+		ny := y + dy[i]
+
+		if nx >= 0 && 10 > nx && ny >= 0 && 10 > ny && !reached[nx][ny] && string(field[nx][ny]) != "x" {
+			dfs(nx, ny, reached)
+		}
+
 	}
 
-	return a
+	return
 }
