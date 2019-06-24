e36eddb5e36b959f2fda734b09dea81181643634
hujimori <hujimori16@gmail.com>
2019-06-24 20:41:02 +0900

    C-GCDonBlackboard解答

--- C-GCDonBlackboard/solve.go
+++ C-GCDonBlackboard/solve.go
@@ -21,57 +21,73 @@ func nextInt() int {
 func main() {
 	sc.Split(bufio.ScanWords)
 	n := nextInt()
-	slice := make([]int, n)
+	a := make([]int, n)
 	for i := 0; i < n; i++ {
-		slice[i] = nextInt()
+		a[i] = nextInt()
+	}
+	left := make([]int, n+1)
+	right := make([]int, n+1)
+
+	for i := 0; i < n; i++ {
+		left[i+1] = gcd(left[i], a[i])
 	}
 
-	max := -1
-
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			if i == j {
-				continue
-			}
-
-			res := gcd(slice[i], slice[j])
-
-			if max < res {
-				max = res
-			}
-		}
+	for i := n - 1; i >= 0; i-- {
+		right[i] = gcd(right[i+1], a[i])
 	}
-	fmt.Println(max)
+
+	res := 0.0
+	for i := 0; i < n; i++ {
+		l := left[i]
+
+		r := right[i+1]
+
+		chmax(&res, float64(gcd(l, r)))
+	}
+	fmt.Println(int(res))
+
+	// slice := make([]int, n)
+	// for i := 0; i < n; i++ {
+	// 	slice[i] = nextInt()
+	// }
+
+	// max := -1
+
+	// for i := 0; i < n; i++ {
+	// 	for j := 0; j < n; j++ {
+	// 		if i == j {
+	// 			continue
+	// 		}
+
+	// 		res := gcd(slice[i], slice[j])
+
+	// 		if max < res {
+	// 			max = res
+	// 		}
+	// 	}
+	// }
 }
 
 func gcd(a, b int) int {
-
-	if a < b {
-		tmp := a
-		a = b
-		b = tmp
-	}
-
-	r := a % b
-	for r != 0 {
-		a = b
-		b = r
-		r = a % b
-	}
-
-	return b
-}
-
-func max(a, b int) int {
-	if a < b {
-		return b
+	if b != 0 {
+		return gcd(b, a%b)
 	}
 	return a
+
 }
 
-func min(a, b int) int {
-	if a < b {
-		return a
+func chmin(a *float64, b float64) bool {
+	if *a > b {
+		*a = b
+		return true
 	}
-	return b
+	return false
+}
+
+func chmax(a *float64, b float64) bool {
+	if *a < b {
+		*a = b
+		return true
+	}
+	return false
 }
