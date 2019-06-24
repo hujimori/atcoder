814aebdcf8bf8bc8daaed3bb41536533a91e1057
hujimori <hujimori16@gmail.com>
2019-06-24 16:48:13 +0900

    C-MultipleGift解答

---
+++ C-MultipleGift/solve.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+
+	x, y := nextInt(), nextInt()
+
+	count := 0
+
+	for x <= y {
+		x *= 2
+		count++
+	}
+
+	fmt.Println(count)
+}
