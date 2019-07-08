3df326bc5bc01f35f394996e189efbaffa1e8865
hujimori <hujimori16@gmail.com>
2019-07-08 17:14:58 +0900

    C-www解答

--- C-www/solve.go
+++ C-www/solve.go
@@ -25,89 +25,79 @@ func main() {
 	sc.Split(bufio.ScanWords)
 	n = nextInt()
 	v = make([]int, n)
-	m := make(map[int]int)
+	m1 := make(map[int]int)
+	m2 := make(map[int]int)
+
 	for i := 0; i < n; i++ {
 		v[i] = nextInt()
-		m[v[i]]++
-	}
-
-	if len(m) == 1 {
-		fmt.Println(n / 2)
-		return
-	}
-
-	oddMax := -1
-	evenMax := -1
-
-	for i := 0; i < n; i++ {
 		if i%2 == 0 {
-			if evenMax < m[v[i]] {
-				evenMax = v[i]
-			}
+			m1[v[i]]++
 		} else {
-			if oddMax < m[v[i]] {
-				oddMax = v[i]
-			}
+			m2[v[i]]++
 		}
 	}
 
-	// fmt.Printf("evenmax=%d, oddmax=%d\n", evenMax, oddMax)
+	if len(m1) == 1 && len(m2) == 1 {
+		flag := true
 
-	cnt := 0
-	for i := 0; i < n; i++ {
-		if i%2 == 0 {
-			if v[i] != evenMax {
-				cnt++
-			}
-		} else {
-			if v[i] != oddMax {
-				cnt++
+		for i := 0; i < n-1; i++ {
+			if v[i] != v[i+1] {
+				flag = false
+				break
 			}
 		}
+
+		if flag {
+			fmt.Println(n / 2)
+			return
+		}
 	}
-	fmt.Println(cnt)
 
-	// fmt.Println(m)
+	// fmt.Println(m1)
+	// fmt.Println(m2)
 
-	// cnt := 0
-	// for i := 0; i < len(v)/2; i++ {
-	// 	cnt += dfs(i, 0, v)
-	// }
-	//  else {
-	// 		fmt.Println(cnt)
-	// 	}
-	// fmt.Println(cnt)
+	em := -1
+	ek := 0
+	for k, v := range m1 {
+		if em < v {
+			em = v
+			ek = k
+		}
+	}
+
+	om := -1
+	for k, v := range m2 {
+		if om < v && ek != k {
+			om = v
+		}
+	}
+	ans1 := n - em - om
+
+	om = -1
+	ok := -1
+	for k, v := range m2 {
+		if om < v {
+			om = v
+			ok = k
+		}
+	}
+
+	em = -1
+	for k, v := range m1 {
+		if em < v && ok != k {
+			em = v
+		}
+	}
+
+	ans2 := n - em - om
+
+	fmt.Println(min(ans1, ans2))
 }
 
-func dfs(i, cnt int, v []int) int {
-	if i >= n {
-		return cnt
+func min(a, b int) int {
+	if a < b {
+		return a
 	}
 
-	var res int
-
-	if i+2 < n && v[i] != v[i+2] {
-		tmp1 := v[i]
-		tmp2 := v[i+2]
-
-		v[i] = tmp2
-		chmin(&res, dfs(i+2, cnt+1, v))
-
-		v[i] = tmp1
-		v[i+2] = tmp1
-		chmin(&res, dfs(i+2, cnt+1, v))
-	} else {
-		chmin(&res, dfs(i+2, cnt, v))
-	}
-
-	fmt.Println(res)
-	return res
-}
-
-func chmin(a *int, b int) bool {
-	if *a > b {
-		*a = b
-		return true
-	}
-	return false
+	return b
 }
