a833e2a4622cd833cee17675aad7c5ae03332f87
hujimori <hujimori16@gmail.com>
2019-07-09 13:58:39 +0900

    C-$-adjacent解答△

--- C-4-adjacent/solve.go
+++ C-4-adjacent/solve.go
@@ -19,85 +19,38 @@ func nextInt() int {
 }
 
 var n int
-var k int
 var a []int
 
 func main() {
 	sc.Split(bufio.ScanWords)
 	n = nextInt()
-	k = 2
 	a = make([]int, n)
-	sum := 1
-	even := 0
 	for i := 0; i < n; i++ {
 		a[i] = nextInt()
-		if a[i]%2 == 0 {
-			even++
-		}
-		sum *= a[i]
 	}
-	fmt.Println()
-
-	cnt := 0
-	for sum != 0 {
-		sum /= 2
-		cnt++
-		if cnt >= n && even >= n/2 {
-			fmt.Println("Yes")
-			return
-		}
-	}
-
-	fmt.Println("No")
-
-	// fmt.Println(e)
-
-	// var slice [][]int
-
-	// for bit := uint(0); bit < (1 << uint(n)); bit++ {
-	// 	s := []int{}
-	// 	for i := uint(0); i < uint(n); i++ {
-	// 		if bit&(1<<i) == 1<<i {
-	// 			s = append(s, a[i])
-	// 		}
-	// 	}
-	// 	slice = append(slice, s)
-	// }
-
-	// solve()
-}
-
-func nextCombination(sub int) int {
-	x := sub & -sub
-	y := sub + x
-	return (((sub & ^y) / x) >> 1) | y
-}
-
-func solve() {
-
-	bit := (1<<uint(k) - 1)
-	slice := [][]int{}
-	for ; bit < (1 << uint(n)); bit = nextCombination(bit) {
-		s := []int{}
-		for i := 0; i < n; i++ {
-			if bit&(1<<uint(i)) == 1<<uint(i) {
-				s = append(s, a[i])
-			}
-		}
-		slice = append(slice, s)
-	}
-	cnt := 0
-	for _, s := range slice {
-		if s[0]*s[1]%4 == 0 {
-			fmt.Println(s)
-			cnt++
-		}
-	}
-
-	if cnt >= n-1 {
+	if solve() {
 		fmt.Println("Yes")
 	} else {
 		fmt.Println("No")
 	}
+}
 
+func solve() bool {
+	c4 := 0
+	c := 0
+
+	for i := 0; i < n; i++ {
+		if a[i]%4 == 0 {
+			c4++
+		} else if a[i]%2 == 1 {
+			c++
+		}
+	}
+
+	if c4+1 == c && n == (c4+c) {
+		return true
+	} else if c4 < c {
+		return false
+	}
+	return true
 }
