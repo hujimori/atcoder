14e321ae2a7f461179b752fcca19f3c6301ce100
hujimori <hujimori16@gmail.com>
2019-07-24 10:57:00 +0900

    D-多重ループ解答

---
+++ D-多重ループ/solve.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+const MAX = 210000
+const MOD = 1000000007
+
+var fac, finv, inv [MAX]int64
+
+func COMinit() {
+	fac[0], fac[1] = 1, 1
+	finv[0], finv[1] = 1, 1
+	inv[1] = 1
+	for i := int64(2); i < MAX; i++ {
+		fac[i] = fac[i-1] * i % MOD
+		inv[i] = MOD - inv[MOD%i]*(MOD/i)%MOD
+		finv[i] = finv[i-1] * inv[i] % MOD
+	}
+}
+
+func COM(n, k int64) int64 {
+	if n < k {
+		return 0
+	}
+	if n < 0 || k < 0 {
+		return 0
+	}
+	return fac[n] * (finv[k] * finv[n-k] % MOD) % MOD
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	n, k := int64(nextInt()), int64(nextInt())
+	COMinit()
+	fmt.Println(COM(n+k-1, k))
+}
