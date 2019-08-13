e75dbab7b04ebc8bbe73c28a1df5fcb64c0710da
hujimori <hujimori16@gmail.com>
2019-08-13 09:51:39 +0900

    D-SummerVacation時直し

--- ABC137/D/solve.go
+++ ABC137/D/solve.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
 	"os"
-	"sort"
 	"strconv"
 )
 
@@ -24,49 +24,43 @@ func nextLine() string {
 	return sc.Text()
 }
 
-type pair struct {
-	first  int
-	second int
-}
+type PriorityQueue []int
 
-type SortBy []pair
-
-func (a SortBy) Len() int      { return len(a) }
-func (a SortBy) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
-func (a SortBy) Less(i, j int) bool {
-
-	if a[i].second > a[j].second {
-		return true
-	} else if a[i].second == a[j].second {
-		if a[i].first > a[j].first {
-			return true
-		}
-	}
-
-	return false
+func (pq PriorityQueue) Len() int            { return len(pq) }
+func (pq PriorityQueue) Less(i, j int) bool  { return pq[i] > pq[j] }
+func (pq PriorityQueue) Swap(i, j int)       { pq[i], pq[j] = pq[j], pq[i] }
+func (pq *PriorityQueue) Push(x interface{}) { *pq = append(*pq, x.(int)) }
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	x := old[n-1]
+	*pq = old[0 : n-1]
+	return x
 }
 
 func main() {
 	sc.Split(bufio.ScanWords)
+
 	N, M := nextInt(), nextInt()
+	ts := make(map[int][]int)
 
-	pairs := make([]pair, N)
 	for i := 0; i < N; i++ {
-		pairs[i].first = nextInt()
-		pairs[i].second = nextInt()
+		a, b := nextInt(), nextInt()
+		ts[a] = append(ts[a], b)
 	}
 
-	// sort.Sort(sort.Reverse(SortBy(pairs)))
-	sort.Sort(SortBy(pairs))
-	// fmt.Println(pairs)
+	pq := make(PriorityQueue, 0, N)
+	heap.Init(&pq)
 	ans := 0
-	for i := 0; i < N; i++ {
-		if pairs[i].first <= M {
-			ans += pairs[i].second
-			M--
+	for i := 1; i <= M; i++ {
+		if _, ok := ts[i]; ok {
+			for _, v := range ts[i] {
+				heap.Push(&pq, v)
+			}
 		}
-
+		if len(pq) > 0 {
+			ans += heap.Pop(&pq).(int)
+		}
 	}
-
 	fmt.Println(ans)
 }
