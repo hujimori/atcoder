2c2066a8c9486d0e082c06968d1dfa34bc05cf69
hujimori <hujimori16@gmail.com>
2019-08-16 15:01:37 +0900

    A-PrefixAndSuffix解答

---
+++ A-PrefixAndSuffix/solve.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func main() {
+	// sc.Split(bufio.ScanWords)
+
+	var N int
+	fmt.Scan(&N)
+	var s, t string
+	fmt.Scan(&s)
+	fmt.Scan(&t)
+
+	// ss := strings.Split(s, "")
+	// tt := strings.Split(t, "")
+
+	for l := 0; l < N; l++ {
+		if s[l:N] == t[0:N-l] {
+			fmt.Println(l + N)
+			return
+		}
+	}
+
+	fmt.Println(N * 2)
+}
